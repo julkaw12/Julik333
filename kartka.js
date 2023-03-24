96e43dee0fa7049eda87277c256440df987bfb5d
julkaw12 <127488698+julkaw12@users.noreply.github.com>
2023-03-24 10:34:32 +0100

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,33 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+kaboom();
+
+loadSprite("kartka","Christmas.png")
+loadSprite("gwiazda","gwiazda.png")
+loadSound("muzyka","muzyyka111.mp3")
+
+add([
+    sprite("kartka")
+])
+
+
+const gwiazda = add([
+    sprite("gwiazda"),
+    pos(0,30)
+])
+
+
+onMouseRelease(()=>play("muzyka"))
+
+let wprawo = true
+
+onUpdate(()=>{
+    if(wprawo && gwiazda.pos.x < 600)
+         gwiazda.pos.x += 1
+    
+    else if(!wprawo && gwiazda.pos.x > 50)
+        gwiazda.pos.x -= 1
+
+        else wprawo ^=true
+
+})
